08045f2d0f2d4117879efebd98537fd8db2b0e07
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-11-03 12:57:28 +0100

    implement settings provider

--- src/PasetoPackage.php
+++ src/PasetoPackage.php
@@ -5,14 +5,14 @@ namespace Bone\Paseto;
 use Barnacle\Container;
 use Barnacle\Exception\NotFoundException;
 use Barnacle\RegistrationInterface;
+use Bone\Console\Command;
+use Bone\Contracts\Container\DefaultSettingsProviderInterface;
 use ParagonIE\Paseto\Keys\SymmetricKey;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-class PasetoPackage implements RegistrationInterface
+class PasetoPackage implements RegistrationInterface, DefaultSettingsProviderInterface
 {
-    /**
-     * @param Container $c
-     */
-    public function addToContainer(Container $c)
+    public function addToContainer(Container $c): void
     {
         if (!$c->has('bone-paseto')) {
             throw new NotFoundException('Please add a bone-paseto array config with a key `sharedKey`');
@@ -28,4 +28,18 @@ class PasetoPackage implements RegistrationInterface
             return new PasetoService($key);
         });
     }
+
+    public function getRequiredPackages(): array
+    {
+        return [];
+    }
+
+    public function getSettingsFileName(): string
+    {
+        return __DIR__ . '/../data/config/bone-paseto.php';
+    }
+
+    public function postInstall(Command $command, SymfonyStyle $io): void {}
+
+
 }
